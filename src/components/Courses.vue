3c3b51874c00c92d5b5cfaeb5ba6d5112ebc805a
shaikha <jshaikha0@gmail.com>
2024-03-13 00:57:51 +0300

    courses view

--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -1,9 +1,35 @@
 <script>
 export default {
   name: "Courses",
+  methods: {
+    handleChange(e) {
+      this[e.target.id] = e.target.value
+    },
+    async handleSubmit(e) {
+      e.preventDefault()
+      // axios call to the backend
+
+      const res = await axios.post(`${import.meta.env.VITE_BASE_URL}/courses`, {
+        name: this.name,
+        email: this.email,
+      })
+
+      this.name = ""
+      this.email = ""
+    },
+  },
 }
 </script>
 <template>
-  <h1>hi</h1>
+  <input placeholder="search course" @change="value" type="text" /><button
+    @click="search"
+  >
+    search
+  </button>
+  <ul>
+    <li>math</li>
+    <li>pysics</li>
+    <li>chemistry</li>
+  </ul>
 </template>
 <style></style>
